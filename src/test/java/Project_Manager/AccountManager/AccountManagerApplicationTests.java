1f100c079f8a562186c3071792a52716302b58e3
jungdogyeong <degreesir@naver.com>
2025-09-16 19:50:37 +0900

    123

---
+++ src/test/java/Project_Manager/AccountManager/AccountManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package Project_Manager.AccountManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
