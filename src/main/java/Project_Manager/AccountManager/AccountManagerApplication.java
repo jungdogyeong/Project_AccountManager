0b3f8144a7096a59c7722dc7fa3cdf4b4aaa890a
seoyun <seoyun9033@gmail.com>
2025-09-17 20:29:26 +0900

    1234

--- src/main/java/Project_Manager/AccountManager/AccountManagerApplication.java
+++ src/main/java/Project_Manager/AccountManager/AccountManagerApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AccountManagerApplication {
 
 	public static void main(String[] args) {
-        System.out.println("hello");
         SpringApplication.run(AccountManagerApplication.class, args);
-        System.out.println("hello");
 	}
 
 }
