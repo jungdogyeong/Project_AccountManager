bd9dd6ee4aab5e68927bb70ac51d3b521b4d479f
jungdogyeong <degreesir@naver.com>
2025-09-26 02:16:41 +0900

    db 수정 및 파일 리펙토링

--- src/main/java/Project_Manager/AccountManager/domain/UserDomain.java
+++ src/main/java/Project_Manager/AccountManager/domain/UserDomain.java
@@ -3,7 +3,7 @@ package Project_Manager.AccountManager.domain;
 import lombok.Data;
 
 @Data
-public class MemberDomain {
+public class UserDomain {
     private Long id;
     private String loginId;
     private String password;
